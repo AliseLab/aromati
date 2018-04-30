bdf8617a97af5cc204469669e0d560d3c510f5b9
afwbkbc <afwbkbc@inbox.lv>
2018-04-30 21:08:36 +0300

    auto reconnect db

--- modules/2-db.js
+++ modules/2-db.js
@@ -6,71 +6,91 @@ exports.run = function( data, next ) {
 	
 	data.config.db.multipleStatements = true;
 	
-	var sql = Mysql.createConnection( data.config.db );
-	
-	var failed = false;
-	var fail = ( msg ) => {
-		console.log( msg );
-		sql.close();
-		failed = true;
-	}
-	
-	sql.query( 'SELECT `db_version` FROM `_meta`', ( err, results, fields ) => {
-		var update_needed = false;
+	var initialized = false;
+	var init = function() {
+		var failed = false;
+		var fail = ( msg ) => {
+			console.log( 'fail', msg );
+			data.sql.close();
+			failed = true;
+		}
 		
-		if ( err ) {
-			if ( err.code == 'ER_NO_SUCH_TABLE' ) {
-				update_needed = true;
+		data.sql.query( 'SELECT `db_version` FROM `_meta`', ( err, results, fields ) => {
+			var update_needed = false;
+			
+			if ( err ) {
+				if ( err.code == 'ER_NO_SUCH_TABLE' ) {
+					update_needed = true;
+				}
+				else {
+					return fail( err.sqlMessage ? err.sqlMessage : err.toString() );
+				}
 			}
 			else {
-				return fail( err.sqlMessage );
-			}
-		}
-		else {
-			results.forEach( result => {
-				if ( result.db_version != db_version ) {
-					console.log( 'Invalid DB version ( needed ' + db_version + ', found ' + result.db_version + ' ), please migrate' );
-					failed = true;
-				}
-			});
-		}
-		
-		if ( !failed ) {
-			data.sql = sql;
-			
-			if ( update_needed ) {
-				console.log( 'loading fixtures...' );
-				data.fs.readdir( './fixtures', ( err, files ) => {
-					if ( err )
-						return fail( err );
-					else {
-						var loaded = 0;
-						var done = () => {
-							if ( ++loaded == files.length ) {
-								console.log( 'done' );
-								next();
-							}
-						};
-						
-						files.forEach( file => {
-							console.log( '\t' + file );
-							data.fs.readFile( './fixtures/' + file, ( err, data ) => {
-								if ( err )
-									return fail( err );
-								sql.query( data.toString(), ( err, results, fields ) => {
-									if ( err )
-										return fail( err.sqlMessage );
-									done();
-								});
-							});
-						});
+				results.forEach( result => {
+					if ( result.db_version != db_version ) {
+						console.log( 'Invalid DB version ( needed ' + db_version + ', found ' + result.db_version + ' ), please migrate' );
+						failed = true;
 					}
 				});
 			}
-			else
-				next();
-		}
+			
+			if ( !failed ) {
+				
+				if ( update_needed ) {
+					console.log( 'loading fixtures...' );
+					data.fs.readdir( './fixtures', ( err, files ) => {
+						if ( err )
+							return fail( err );
+						else {
+							var loaded = 0;
+							var done = () => {
+								if ( ++loaded == files.length ) {
+									console.log( 'done' );
+									next();
+								}
+							};
+							
+							files.forEach( file => {
+								console.log( '\t' + file );
+								data.fs.readFile( './fixtures/' + file, ( err, data ) => {
+									if ( err )
+										return fail( err );
+									data.sql.query( data.toString(), ( err, results, fields ) => {
+										if ( err )
+											return fail( err.sqlMessage );
+										done();
+									});
+								});
+							});
+						}
+					});
+				}
+				else
+					next();
+			}
 
-	});
+		});
+	}
+	
+	var doconnect = function() {
+		data.sql = Mysql.createConnection( data.config.db );
+		data.sql.on( 'connect', () => {
+			console.log( 'db connected' );
+			if ( !initialized ) {
+				initialized = true;
+				init();
+			}
+		});
+		data.sql.on( 'error', err => {
+			console.log( err.toString() );
+			if ( !err.fatal )
+				return;
+			console.log( 'reconnecting db' );
+			data.sql.close();
+			setTimeout( doconnect, 100 );
+		});
+	};
+	doconnect();
 	
 }
